4541dd7485ab443a925ab52fc7edc5728aa40868
Matilda1232 <78929977+Matilda1232@users.noreply.github.com>
2021-02-11 21:41:02 +0100

    Rock, paper, scissors game. Finished version.

--- Program.cs
+++ Program.cs
@@ -6,12 +6,20 @@ namespace Uppgift09
     {
         static void Main(string[] args)
         {
+            Console.Title = "Rock, paper, scissors";
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            
             Console.WriteLine("Welcome! We're about to play Rock, Paper, Scissors.");
 
             Random number = new Random();
             
+            int playerScore = 0;
+            int computerScore = 0;
 
             Console.WriteLine ("You are playing against the computer. Type 1 for Rock, 2 for Paper or 3 for Scissors!");
+           
+           goAgain:
+
             int player1Choice = Convert.ToInt32(Console.ReadLine ());
 
             switch (player1Choice)
@@ -41,9 +49,47 @@ namespace Uppgift09
                 Console.WriteLine ("The computer chose Scissors!" );
                 break;
             }
-            
-            
 
+            if (player1Choice == computerChoice)
+            {
+                Console.WriteLine ("It's a tie!");
+            }
+             else if (player1Choice == 1 && computerChoice == 3)
+             {
+                 Console.WriteLine ("You won, well done!");
+                 playerScore++;
+             }
+
+            else if (player1Choice == 2 && computerChoice == 1)
+            {
+                Console.WriteLine ("You won, well done!");
+                playerScore++;
+            }
+             else if (player1Choice == 3 && computerChoice == 2)
+            {
+                Console.WriteLine ("You won, well done!");
+                playerScore++;
+            }
+            else
+            {
+                Console.WriteLine ("The computer won! Better luck next time!");
+                computerScore++;
+            }
+        
+        Console.WriteLine ("Do you want to play again? Please answer 'yes' or 'no'");
+        string playAgain = Console.ReadLine ();
+
+        if (playAgain == "yes")
+        {
+            Console.WriteLine ("Welcome back! Type 1 for Rock, 2 for Paper or 3 for Scissors!");
+            goto goAgain;
+        }
+
+        else
+        {
+            Console.WriteLine ("Thanks for playing, see you another time!");
+            Console.WriteLine ("You won " + playerScore + " times. The computer won " + computerScore + " times.");
+        }
 
 
 
